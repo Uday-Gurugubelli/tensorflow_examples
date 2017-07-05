9518f60351060875c8f5311c911cd100e6d0e627
Uday Kumar Gurugubelli <udaygurugubelli@gmail.com>
2017-07-05 21:48:49 +0530

    Generative Adversarial Networks--updated

--- gan.py
+++ gan.py
@@ -1,66 +1,80 @@
 import numpy as np
 import tensorflow as tf
+imort matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
+
 tf.logging.set_verbosity(tf.logging.INFO)
 tf.reset_default_graph()
 
-mnist = input_data.read_data_sets("MNIST_data/")
+FALGS=None
 img_dim = 784
-z_dim=200
-total_damples = len(mnist)
+batch=100
+niter=100
 
-def generator(x):
-    x = tf.layers.dense(inputs=x, units=256, activation=tf.nn.relu)
-    x = tf.layers.dense(inputs=x, units=img_dim, activation=tf.nn.sigmoid)
-    return x
+mnist = input_data.read_data_sets("MNIST_data/")
+test_data = np.random.uniform(0., 1., [batch, img_dim])
+
+def generator(ip):
+    net = tf.layers.dense(inputs=ip, units=512, activation=tf.nn.relu)
+    net = tf.layers.dense(inputs=net, units=1024, activation=tf.nn.relu)
+    net = tf.layers.dense(inputs=net, units=img_dim, activation=tf.nn.sigmoid)
+    return net
 def discriminator(x):
-    x = tf.layers.dense(inputs=x, units=256, activation=tf.nn.relu)
+    x = tf.layers.dense(inputs=x, units=512, activation=tf.nn.relu)
+    x = tf.layers.dense(inputs=x, units=1024, activation=tf.nn.relu)
     x = tf.layers.dense(inputs=x, units=1, activation=tf.nn.sigmoid)
     return x
+
 def model_fn(features, labels, mode):
 
     gen_sample = generator(features)
+    disc_fake = discriminator(gen_sample)
     disc_real = discriminator(gen_sample)
-
+    
+    train_op = None
+    predictions = None
+    loss = tf.convert_to_tensor(0.)
+    global_step = tf.train.get_global_step()
     if(mode == tf.estimator.ModeKeys.EVAL or
             mode == tf.estimator.ModeKeys.TRAIN):
-
-        loss = -tf.reduce_mean(labels*tf.log(disc_real) + labels*tf.log(1. - disc_real))
-    train_op = None
-    global_step = tf.train.get_global_step()
+        loss = -tf.reduce_mean(tf.log(disc_real) + tf.log(1. - disc_fake))
+      
     if(mode == tf.estimator.ModeKeys.TRAIN):
         train_op = tf.train.GradientDescentOptimizer(0.01).minimize(
                             loss, global_step = global_step)
-
-    predictions = {"classes": features,
-                   "labels ": labels}
+    gen_pred = generator(features)    
+    predictions = {"pred_labels ": tf.convert_to_tensor(gen_pred)}
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss,
                                     train_op=train_op, predictions=predictions)
 
-def my_input_fn():
-    len = 10000
-    rand = np.random.randint(0, 2)
-    if(rand == 1):
-        x = tf.random_uniform([len, 784], 0, 1)
-        y = tf.convert_to_tensor(
-                    tf.random_uniform([len, 1], 0, 10, tf.int32))
-        y = tf.cast(tf.tile(y, [1,784]), tf.float32)
-    else:
-        x, y1 = mnist.train.next_batch(len)
-        y = tf.reshape(y1, [len, 1])
-        y = tf.cast(tf.tile(y, [1,784]), tf.float32)
-
-    g, h = tf.train.slice_input_producer([x, y])
-    
-    t1, t2  = tf.train.shuffle_batch_join(tensors_list = [[g, h]],
-                               batch_size=100,
-                               capacity=1000,
-                               min_after_dequeue=500)
-    
+def input_fn():
+    t1, t2 = mnist.train.next_batch(batch)
     return tf.convert_to_tensor(t1), tf.convert_to_tensor(t2)
 
+def plt_ip_fn():
+    [t] = tf.train.slice_input_producer([test_data])
+    x = tf.train.shuffle_batch([t], batch_size=batch, capacity = 500,
+                                        min_after_dequeue=100)
+    y = None
+    return x, y
 
-estimator = tf.estimator.Estimator(model_fn = model_fn)
-for i in range(100):
-    estimator.train(input_fn = my_input_fn, steps = 1)
-
+def main(_):
+    estimator = tf.estimator.Estimator(model_fn = model_fn)
+    for i in range(niter):
+        estimator.train(input_fn = input_fn, steps = 1)
+        
+    estimator.evaluate(input_fn = input_fn, steps = 1)
+    
+    pred = estimator.predict(input_fn = plt_ip_fn)
+    for i, p in enumerate(pred):
+        print(p["labels"])
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_dir', type=str,
+                        default='/tmp/gan_model',
+                        help='path to model dir')
+    FLAGS, unparsed = parser.parse_known_args()
+    
+    
+    main(sys.argv[0])
