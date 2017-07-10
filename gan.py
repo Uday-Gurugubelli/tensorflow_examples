1b2004568719d72e893ad80cf9d47d3b4e048751
Uday Kumar Gurugubelli <udaygurugubelli@gmail.com>
2017-07-10 23:34:48 +0530

    Update gan.py

--- gan.py
+++ gan.py
@@ -8,30 +8,50 @@ tf.reset_default_graph()
 
 FALGS=None
 img_dim = 784
+z_dim = 100
 batch=100
-niter=1000
+test_batch = 20
+niter=50000
 
 mnist = input_data.read_data_sets("MNIST_data/")
-test_data = np.random.uniform(0., 1., [batch, img_dim])
+test_data = np.random.uniform(0., 1., [test_batch, z_dim])
 
-def generator(ip):
-    net = tf.layers.dense(inputs=ip, units=512, activation=tf.nn.relu)
-    net = tf.layers.dense(inputs=net, units=1024, activation=tf.nn.relu)
-    net = tf.layers.dense(inputs=net, units=img_dim, activation=tf.nn.sigmoid)
-    return net
-def discriminator(x):
-    x = tf.layers.dense(inputs=x, units=512, activation=tf.nn.relu)
-    x = tf.layers.dense(inputs=x, units=1024, activation=tf.nn.relu)
-    x = tf.layers.dense(inputs=x, units=1, activation=tf.nn.sigmoid)
-    return x
+def generator(ip, reuse=False):
+    with tf.variable_scope('Generator', reuse=reuse):
+        net = tf.layers.dense(inputs=ip, units=512, activation=tf.nn.tanh,
+                        kernel_initializer= tf.random_uniform_initializer(-1,1),
+                        bias_initializer=tf.zeros_initializer(),
+                        kernel_regularizer=tf.contrib.layers.l2_regularizer(0.001),
+                        bias_regularizer=None)
+        net = tf.layers.dense(inputs=net, units=1024, activation=tf.nn.tanh,
+                        kernel_initializer= tf.random_uniform_initializer(-1,1),
+                        bias_initializer=tf.zeros_initializer(),
+                        kernel_regularizer=tf.contrib.layers.l2_regularizer(0.001),
+                        bias_regularizer=None)
+        net = tf.layers.dense(inputs=net, units=img_dim, activation=tf.nn.sigmoid)
+        return net
+def discriminator(ip, reuse=False):
+    with tf.variable_scope('Discriminator', reuse=reuse):
+        net = tf.layers.dense(inputs=ip, units=512, activation=tf.nn.tanh, 
+                        kernel_initializer= tf.random_uniform_initializer(-1,1),
+                        bias_initializer=tf.zeros_initializer(),
+                        kernel_regularizer=tf.contrib.layers.l2_regularizer(0.001),
+                        bias_regularizer=None)
+        net = tf.layers.dense(inputs=net, units=1024, activation=tf.nn.tanh,
+                        kernel_initializer= tf.random_uniform_initializer(-1,1),
+                        bias_initializer=tf.zeros_initializer(),
+                        kernel_regularizer=tf.contrib.layers.l2_regularizer(0.001),
+                        bias_regularizer=None)
+        net = tf.layers.dense(inputs=net, units=1, activation=tf.nn.sigmoid)
+        return net
 
 def model_fn(features, labels, mode):
 
-    fake = tf.random_uniform([batch, img_dim], 0, 1)
+    fake = tf.random_uniform([batch, z_dim], 0, 1)
     
     gen_sample = generator(fake)
     disc_fake = discriminator(gen_sample)
-    disc_real = discriminator(features)
+    disc_real = discriminator(features, reuse=True)
     
     train_op = None
     predictions = None
@@ -40,15 +60,15 @@ def model_fn(features, labels, mode):
     if(mode == tf.estimator.ModeKeys.EVAL or
             mode == tf.estimator.ModeKeys.TRAIN):
         loss_disc = -tf.reduce_mean(tf.log(disc_real) + tf.log(1. - disc_fake))
-        loss_gen = -tf.reduce_mean(tf.log(gen_sample))
+        loss_gen = -tf.reduce_mean(tf.log(disc_fake))
         loss = loss_disc + loss_gen
       
     if(mode == tf.estimator.ModeKeys.TRAIN):
-        train_op = tf.train.GradientDescentOptimizer(0.01).minimize(
+        train_op = tf.train.GradientDescentOptimizer(0.001).minimize(
                             loss, global_step = global_step)
     if(mode == tf.estimator.ModeKeys.PREDICT):
-        gen_pred = generator(features)    
-        predictions = {"pred_labels ": tf.convert_to_tensor(gen_gen)}
+        gen_pred = generator(features, reuse=True)    
+        predictions = {"pred ": tf.convert_to_tensor(gen_pred)}
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss,
                                     train_op=train_op, predictions=predictions)
 
@@ -57,8 +77,9 @@ def input_fn():
     return tf.convert_to_tensor(t1), tf.convert_to_tensor(t2)
 
 def plt_ip_fn():
-    [t] = tf.train.slice_input_producer([test_data])
-    x = tf.train.shuffle_batch([t], batch_size=batch, capacity = 500,
+    global test_data
+    [t] = tf.train.slice_input_producer([test_data], num_epochs=1)
+    x = tf.train.shuffle_batch([t], batch_size=test_batch, capacity = 500,
                                         min_after_dequeue=100)
     y = None
     return x, y
@@ -67,12 +88,21 @@ def main(_):
     estimator = tf.estimator.Estimator(model_fn = model_fn)
     for i in range(niter):
         estimator.train(input_fn = input_fn, steps = 1)
-        
-    estimator.evaluate(input_fn = input_fn, steps = 1)
     
     pred = estimator.predict(input_fn = plt_ip_fn)
+    op = []
     for i, p in enumerate(pred):
-        print(p["labels"])
+        op.append(p["pred"])
+        print(p["pred"])
+    
+    f, a = plt.subplots(2, 10, figsize=(10, 4))
+
+    for i in range(10):
+        for j in range(2):
+            a[j][i].imshow(np.reshape(op[i], (28, 28)))
+    f.show()
+    plt.draw()
+    plt.waitforbuttonpress()
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -81,4 +111,4 @@ if __name__ == '__main__':
                         help='path to model dir')
     FLAGS, unparsed = parser.parse_known_args()
 
-    
+tf.app.run()
